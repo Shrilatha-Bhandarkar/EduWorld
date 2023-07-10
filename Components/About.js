ff6b28d8f2b4ef08577afe3830cc56d8812b5e84
Shrilatha-Bhandarkar <125753781+Shrilatha-Bhandarkar@users.noreply.github.com>
2023-07-10 15:46:14 +0530

    Create About.js

---
+++ Components/About.js
@@ -0,0 +1,76 @@
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+const About=()=>{
+    return(
+      <>
+        <div class="container border border-5">
+        <div class="row">                      
+             <div class="col-md bg-info"><center>         
+                <h1>About us</h1></center>  
+             </div>    
+        </div>
+        </div>
+        <dic class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card bg-info-subtle">
+                    <img src="https://cdn2.vectorstock.com/i/1000x1000/76/01/education-icon-male-student-person-profile-avatar-vector-25957601.jpg"
+                    alt="loading..."
+                    height="450px"
+                    />
+                    <div class="card-body">
+                        <h1 class="card-header">Mr.sdnjcisude</h1>
+                        <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                         Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley 
+                         of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                          leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the 
+                          release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                           like Aldus PageMaker including versions of Lorem Ipsum. </p>
+                            
+                            <a href="https://www.lipsum.com/">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-info-subtle">
+                    <img src="https://cdn2.vectorstock.com/i/1000x1000/76/01/education-icon-male-student-person-profile-avatar-vector-25957601.jpg"
+                    alt="loading..."
+                    height="450px"
+                    />
+                    <div class="card-body">
+                        <h1 class="card-header">Mr.Qiuejwijd</h1>
+                        <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                         Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley 
+                         of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                          leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the 
+                          release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                           like Aldus PageMaker including versions of Lorem Ipsum. </p>
+                            <a href="https://www.lipsum.com/">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-info-subtle">
+                    <img src="https://cdn2.vectorstock.com/i/1000x1000/76/01/education-icon-male-student-person-profile-avatar-vector-25957601.jpg"
+                    alt="loading..."
+                    height="450px"
+                    />
+                    <div class="card-body">
+                        <h1 class="card-header">Mr.Qiuejwijd</h1>
+                        <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                         Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley 
+                         of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                          leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the 
+                          release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                           like Aldus PageMaker including versions of Lorem Ipsum. </p>
+                            <a href="https://www.lipsum.com/">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </dic>
+  </>
+    )
+}
+
+export default About;
